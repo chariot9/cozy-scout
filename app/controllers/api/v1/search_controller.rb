bcf64e699ce6a37c8bd2025aa0e0e1ba34efcda8
chariot9 <trungvu.inside@gmail.com>
2019-04-04 01:37:48 +0900

    Rename controller

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class PostsController < ApplicationController
+    class SearchController < ApplicationController
 
       # TODO(trung) Add more params here
       BASIC_PARAMS = [
